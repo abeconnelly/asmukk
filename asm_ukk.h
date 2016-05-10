ed50c813afc5b882eab416d1460920b3347b7b61
abram <abram@curoverse.com>
2016-05-09 23:13:38 -0400

    cruft

--- asm_ukk.h
+++ asm_ukk.h
@@ -8,20 +8,6 @@
 #define ASM_UKK_MISMATCH 3
 #define ASM_UKK_GAP 2
 
-typedef struct asm_ukk_type {
-  char *SeqA, *SeqB;
-  char *AlignA, *AlignB;
-  int Threshold;
-  int Score;
-  int *W;
-  int w_len;
-  int n_r, n_c;
-
-  int MismatchCost, GapCost;
-} asm_ukk_t;
-
-asm_ukk_t AsmUkkDefaultContext;
-
 int asm_ukk_score(char *, char *);
 int asm_ukk_score2(char *, char *, int, int);
 
